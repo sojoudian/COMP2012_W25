d094c8714dab7644060a34def4be7e90e01c6fb2
maziar <sojodyan@gmail.com>
2025-01-28 15:52:56 -0500

    lec04

---
+++ Week04/lecture/lec04.py
@@ -0,0 +1,15 @@
+import os
+
+file_name = "file.txt"
+
+if os.path.exists(file_name):
+    with open(file_name, "r") as textfile: #text file is an alias for file_name
+        content = textfile.read()
+        if content.strip():
+            print("File content:")
+            print(content)
+
+else:
+    print("File not existed")
+
+# open()
